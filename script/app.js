377ade1decae0cafcf20bdeee0e4141198a34342
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-11-27 13:56:24 +0100

    init gallery

--- script/app.js
+++ script/app.js
@@ -1,6 +1,38 @@
 const header = document.querySelector('header')
 const navbar = document.querySelector('.navbar')
+const galleryImgs = document.querySelectorAll('.gallery-img')
+const lightboxContainer = document.querySelector('.lightbox-img-container')
+const lightboxImg = document.querySelector('.lightbox-image')
+const blurBg = document.querySelector('.blur-bg')
 
-window.addEventListener('resize', () => {
-    navbar.style.top=`${header.offsetHeight}px`
-})
+function headerPosition() {
+    navbar.style.top = `${header.offsetHeight}px`
+}
+
+addEventListener('load', headerPosition)
+window.addEventListener('resize', headerPosition)
+
+function showLightboxImg(e) {
+    let scrollPositon = window.scrollY
+    lightboxContainer.innerHTML = `<img class="lightbox-image" src="${e.target.src}" alt="">`
+    lightboxContainer.classList.toggle('hide')
+    document.body.classList.toggle('scroll-disable')
+    document.body.style.top = `-${scrollPositon}px`
+    blurBg.classList.toggle('blur-bg-active')
+}
+
+galleryImgs.forEach(img => img.addEventListener('click', showLightboxImg))
+
+function hideLightboxImg() {
+    let scrollValue = Math.abs(parseInt(document.body.style.top))
+    document.querySelector('.lightbox-image').classList.add('hide')
+    lightboxContainer.classList.toggle('hide')
+    blurBg.classList.toggle('blur-bg-active')
+    document.body.classList.toggle('scroll-disable')
+    window.scrollTo({
+        top: scrollValue,
+        behavior: 'instant'
+    })
+}
+
+lightboxContainer.addEventListener('click', hideLightboxImg)
